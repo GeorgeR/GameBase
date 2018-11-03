9e5a7b480ee95f31497ad2f917d02309abc7869e
GeorgeR <dukecg@gmail.com>
2018-11-03 21:41:05 +1100

    4.20 compat

--- Source/GameBaseEditor/GameBaseEditor.Build.cs
+++ Source/GameBaseEditor/GameBaseEditor.Build.cs
@@ -5,16 +5,19 @@ public class GameBaseEditor : ModuleRules
 	public GameBaseEditor(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				"GameBaseEditor/Public"
-			});
 
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				"GameBaseEditor/Private",
-			});
+	    if (Target.Version.MinorVersion <= 19)
+	    {
+            PublicIncludePaths.AddRange(
+			    new string[] {
+				    "GameBaseEditor/Public"
+			    });
+
+		    PrivateIncludePaths.AddRange(
+			    new string[] {
+				    "GameBaseEditor/Private",
+			    });
+        }
 
 		PublicDependencyModuleNames.AddRange(
 			new string[]
