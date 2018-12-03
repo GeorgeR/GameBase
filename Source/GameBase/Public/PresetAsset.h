c40618af0f55927a7e6405d43c580d9cd8728aa4
George <dukecg@gmail.com>
2018-12-04 07:26:45 +1100

    a

---
+++ Source/GameBase/Public/PresetAsset.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "PresetAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEBASE_API UPresetAsset : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
