1661e54861344e5e71b68cb8972a88ed3977b620
GeorgeR <dukecg@gmail.com>
2018-12-09 16:51:22 +1100

    hash fixes

--- Source/GameBase/Private/Hash.cpp
+++ Source/GameBase/Private/Hash.cpp
@@ -1,42 +1,109 @@
 #include "Hash.h"
 
-float FHash::Hash(const FVector2D Point)
+/* 1D */
+float FHash::Hash1D(const float X)
 {
-    return Hash(Point.X, Point.Y);
+    return FMath::Frac(1.0e4 * FMath::Sin(17.0f * X) * (0.1f + FMath::Abs(FMath::Sin(13.0f * X))));
 }
 
-float FHash::Hash(const float X, const float Y)
+float FHash::Hash1D(const int32 X)
+{
+    return FMath::Frac(1.0e4 * FMath::Sin(17.0f * X) * (0.1f + FMath::Abs(FMath::Sin(13.0f * X))));
+}
+
+/* 2D */
+float FHash::Hash2D(const FVector2D Point)
+{
+    return Hash2D(Point.X, Point.Y);
+}
+
+float FHash::Hash2D(const float X, const float Y)
 {
     return FMath::Frac(1.0e4 * FMath::Sin(17.0f * X + 0.1f * Y) * (0.1f + FMath::Abs(FMath::Sin(13.0f * Y + X))));
 }
 
-float FHash::Hash(const FVector Point)
+float FHash::Hash2D(const FIntPoint Point)
 {
-    return Hash(Point.X, Point.Y, Point.Z);
+    return Hash2D(Point.X, Point.Y);
 }
 
-float FHash::Hash(const float X, const float Y, const float Z)
+float FHash::Hash2D(const int32 X, const int32 Y)
 {
-    auto XY = Hash(X, Y);
-    return Hash(XY, Z);
+    return FMath::Frac(1.0e4 * FMath::Sin(17.0f * X + 0.1f * Y) * (0.1f + FMath::Abs(FMath::Sin(13.0f * Y + X))));
 }
 
-float UHashFunctionLibrary::Hash_2D_Point(const FVector2D Point)
+/* 3D */
+float FHash::Hash3D(const FVector Point)
 {
-    return FHash::Hash(Point);
+    return Hash3D(Point.X, Point.Y, Point.Z);
 }
 
-float UHashFunctionLibrary::Hash_2D(const float X, const float Y)
+float FHash::Hash3D(const float X, const float Y, const float Z)
 {
-    return FHash::Hash(X, Y);
+    auto XY = Hash2D(X, Y);
+    return Hash2D(XY, Z);
 }
 
-float UHashFunctionLibrary::Hash_3D_Point(const FVector Point)
+float FHash::Hash3D(const FIntVector Point)
 {
-    return FHash::Hash(Point);
+    return Hash3D(Point.X, Point.Y, Point.Z);
 }
 
-float UHashFunctionLibrary::Hash_3D(const float X, const float Y, const float Z)
+float FHash::Hash3D(const int32 X, const int32 Y, const int32 Z)
 {
-    return FHash::Hash(X, Y, Z);
+    auto XY = Hash2D(X, Y);
+    return Hash2D(XY, (float)Z);
+}
+
+/* 1D */
+float UHashFunctionLibrary::Hash_1D_F(const float X)
+{
+    return FHash::Hash1D(X);
+}
+
+float UHashFunctionLibrary::Hash_1D_I(const int32 X)
+{
+    return FHash::Hash1D(X);
+}
+
+/* 2D */
+float UHashFunctionLibrary::Hash_2D_Point_F(const FVector2D Point)
+{
+    return FHash::Hash2D(Point);
+}
+
+float UHashFunctionLibrary::Hash_2D_F(const float X, const float Y)
+{
+    return FHash::Hash2D(X, Y);
+}
+
+float UHashFunctionLibrary::Hash_2D_Point_I(const FIntPoint Point)
+{
+    return FHash::Hash2D(Point);
+}
+
+float UHashFunctionLibrary::Hash_2D_I(const int32 X, const int32 Y)
+{
+    return FHash::Hash2D(X, Y);
+}
+
+/* 3D */
+float UHashFunctionLibrary::Hash_3D_Point_F(const FVector Point)
+{
+    return FHash::Hash3D(Point);
+}
+
+float UHashFunctionLibrary::Hash_3D_F(const float X, const float Y, const float Z)
+{
+    return FHash::Hash3D(X, Y, Z);
+}
+
+float UHashFunctionLibrary::Hash_3D_Point_I(const FIntVector Point)
+{
+    return FHash::Hash3D(Point);
+}
+
+float UHashFunctionLibrary::Hash_3D_I(const int32 X, const int32 Y, const int32 Z)
+{
+    return FHash::Hash3D(X, Y, Z);
 }
