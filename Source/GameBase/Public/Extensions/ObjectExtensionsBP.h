4616158d0355acd65f3a0fab28820f2b0ff2b033
GeorgeR <dukecg@gmail.com>
2019-02-05 21:32:42 +1100

    Added object extensions

---
+++ Source/GameBase/Public/Extensions/ObjectExtensionsBP.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ObjectExtensionsBP.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEBASE_API UObjectExtensionsBP : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
